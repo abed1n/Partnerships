eb247265403f73751b1c48d1f393c014a763b26c
Acer <Acer@DESKTOP-REAV2SK>
2026-01-12 16:03:34 +0100

    spremno za rad

--- Partnerships/src/Main.java
+++ Partnerships/src/Main.java
@@ -1,8 +1,17 @@
 
 public class Main {
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Zdravo svijete");
-		System.out.println("skdnfksdkf");
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
+	
 }
